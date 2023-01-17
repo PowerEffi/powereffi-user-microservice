98e60903832095ee23772cbc2596cb06b0fe6a56
Bruno Telles Farsura <bruno.telles.farsura@accenture.com>
2023-01-17 19:44:27 -0300

    Add new test for POST and GET

--- __tests__/pages/api/user.test.ts
+++ __tests__/pages/api/user.test.ts
@@ -8,7 +8,7 @@ import {describe, expect, it} from '@jest/globals';
 
 describe('/api/user API Endpoint', () => {
 
-  function mockRequestResponse(method: RequestMethod = 'POST') {
+  function mockRequestResponsePost(method: RequestMethod = 'POST') {
     const {
       req,
       res,
@@ -20,7 +20,7 @@ describe('/api/user API Endpoint', () => {
   }
 
   it('should not create new user for unauthorized call', async () => {
-    const { req, res } = mockRequestResponse();
+    const { req, res } = mockRequestResponsePost();
 
     req.body = {
       "name": "Dummy Name",
@@ -33,5 +33,48 @@ describe('/api/user API Endpoint', () => {
 
     await userHandler(req, res);
     expect(res.statusCode).toBe(401);
+  });
+
+  it('should create new user', async () => {
+    const { req, res } = mockRequestResponsePost();
+
+    req.body = {
+      "name": "Dummy Name",
+      "phone": 11223344556,
+      "documentNumber": "112233445566",
+      "email": "email@host.com",
+      "password": "Admin@123",
+      "ativo": true
+    }
+
+    req.headers.authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2M2MDdjZmNkZTk5NjhmODAwOTI5NGUiLCJpYXQiOjE2NzM5OTQ2ODV9.zSKCms5gWpRCFXxbf9pqb2kC2-JmwHiBhQaTT3JiN9o"
+
+    await userHandler(req, res);
+    expect(res.statusCode).toBe(200);
+  });
+
+});
+
+describe('/api/user API Endpoint', () => {
+  function mockRequestResponseGet(method: RequestMethod = 'GET') {
+    const {
+      req,
+      res,
+    }: { req: NextApiRequest; res: NextApiResponse } = createMocks({ method });
+    req.headers = {
+      'Content-Type': 'application/json'
+    },
+      req.query.userId = "21312"
+    return { req, res };
+  }
+
+  it('should not get user for unauthorized call', async () => {
+    const { req, res } = mockRequestResponseGet();
+
+    req.headers.authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2M2MDdjZmNkZTk5NjhmODAwOTI5NGUiLCJpYXQiOjE2NzM5OTQ2ODV9.zSKCms5gWpRCFXxbf9pqb2kC2-JmwHiBhQaTT3JiN9o"
+    req.query.id = "63c721c779bff5c41388c203"
+
+    await userHandler(req, res);
+    expect(res.statusCode).toBe(200);
   });
 });
